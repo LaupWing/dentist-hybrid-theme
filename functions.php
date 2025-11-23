948d34af19a374b9e2f84366931099cf553bc147
laupwing <laupwing@gmail.com>
2025-11-23 16:17:40 +0100

    Disable WordPress Admin Bar on the frontend

--- functions.php
+++ functions.php
@@ -3,6 +3,9 @@
  * Dentist Hybrid Theme Functions
  */
 
+// Disable WordPress Admin Bar on Frontend
+add_filter('show_admin_bar', '__return_false');
+
 // Enqueue Tailwind CSS
 function dentist_hybrid_enqueue_styles() {
     // Enqueue Oswald font from Google Fonts
